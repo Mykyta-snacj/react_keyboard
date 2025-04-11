7130e7750d5e09d3480e03b6c07aad6e202c7aa5
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-04-11 11:32:58 +0200

    add a task solution

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,31 @@ export class App extends React.Component<State> {
     pressedKey: '',
   };
 
-  handleKeyUp = (event: KeyboardEvent) => {
+  onKeyUp = (event: KeyboardEvent) => {
     this.setState({
       pressedKey: event.key,
     });
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', this.handleKeyUp);
+    document.addEventListener('keyup', this.onKeyUp);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keyup', this.handleKeyUp);
+    document.removeEventListener('keyup', this.onKeyUp);
   }
 
   render() {
-    if (this.state.pressedKey !== '') {
-      return (
-        <div className="App">
+    return (
+      <div className="App">
+        {this.state.pressedKey === '' ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
           <p className="App__message">
             The last pressed key is [{this.state.pressedKey}]
           </p>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <p className="App__message">Nothing was pressed yet</p>
-        </div>
-      );
-    }
+        )}
+      </div>
+    );
   }
 }
